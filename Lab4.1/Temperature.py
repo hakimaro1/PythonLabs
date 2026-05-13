5220115c21ec5334960c9e4f901184a4988d4da7
hakimaro1 <sanya.kashirin1969@bk.ru>
2026-05-13 13:30:14 +0300

    Контрольная 4.1

---
+++ Lab4.1/Temperature.py
@@ -0,0 +1,7 @@
+print("Введите температуру по Цельсию: ")
+Celcius =float (input())
+print (Celcius * (9 / 5 ) + 32)
+
+print("Введите температуру по Фаренгейту: ")
+Farenheit =float (input())
+print ((Farenheit - 32) * 5 /9)
