3d32e85d85baa8ccbc36138c3ac8ef10311e9db0
hakimaro1 <sanya.kashirin1969@bk.ru>
2026-05-15 10:07:54 +0300

    Добавлены разные способы форматирования

--- Lab4.2/main.py
+++ Lab4.2/main.py
@@ -4,4 +4,7 @@ Tax = Summ * 13 / 100
 print(f"Налог на сумму: {Tax:.2f}")
 Tip = Summ * 10 / 100
 print(f"Чаевые официанту: {Tip:.2f}")
-print(f"Ощая сумма: {(Summ + Tax + Tip):.2f}")
+##Разные варианты форматирования
+print(f"Общая сумма: {(Summ + Tax + Tip):.2f}")
+print("Общая сумма: {:7.2f} ".format(Summ +Tax + Tip))
+
